c899de5ca5e43f5adadaaae5d725ba3e517ff2c7
dolvyx <anrusilkoton@gmail.com>
2025-09-14 16:59:48 +0300

    feat: add first 4 problems

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -18,8 +18,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 /**
@@ -33,8 +33,8 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return Math.PI * radius * 2;
 }
 
 /**
@@ -49,8 +49,8 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return value1 / 2 + value2 / 2;
 }
 
 /**
@@ -67,8 +67,22 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  let a = 0;
+  let b = 0;
+  if (x1 > x2) {
+    a = x1 - x2;
+  }
+  if (x2 > x1) {
+    a = x2 - x1;
+  }
+  if (y1 > y2) {
+    b = y1 - y2;
+  }
+  if (y2 > y1) {
+    b = y2 - y1;
+  }
+  return Math.sqrt(a ** 2 + b ** 2);
 }
 
 /**
